5b4426e859507a39263a8ac381c084b0b28b485d
Adrien Lemaire <ladrien2@gmail.com>
2018-06-18 15:11:35 +0200

    Create a first UI to Homepage

--- components/Views/HomePage.js
+++ components/Views/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Dimensions, ScrollView, Image, ActivityIndicator, TouchableNativeFeedback } from 'react-native';
+import { StyleSheet, Text, View, Image, Animated, TextInput, TouchableNativeFeedback, ScrollView, Dimensions } from 'react-native';
 
 class HomePage extends React.Component {
 
@@ -32,11 +32,97 @@ class HomePage extends React.Component {
         else{
             return(
                 <View style={{flex:1,width:screenWidth, height:screenHeight,alignItems: 'center', justifyContent: 'center'}}>
-                    <Text style={{fontSize:25}}>Chargement terminé</Text>
+                    <View style={styles.container}>
+                        <Image source={require('../../icons/loading-start.gif')} style={{width: 90, height: 90}}/>
+                    </View>
+                    <View style={{flexDirection:'row',alignItems: 'center',justifyContent: 'center',position:'relative',top:-25}}>
+                        <TouchableNativeFeedback style={{flex:1,flexDirection:'row',alignItems: 'center',justifyContent: 'center',padding:5}}>
+                        <View style={styles.searchBar}>
+                            <Image source={require('../../icons/search.png')} style={styles.ImageStyle} />
+                            <Text style={styles.input}>Où souhaitez-vous aller ?</Text>
+                        </View>
+                        </TouchableNativeFeedback >
+                    </View>
+                    <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Vos arrêts</Text>
+                    <ScrollView horizontal={true}>
+                        <View style={{flex:1,backgroundColor:'red',width:screenWidth/3,height:100}}>
+                        <Text>95-01</Text>
+                        </View>
+                        <View style={{flex:1,backgroundColor:'blue',width:screenWidth/3,height:100}}>
+                        <Text>95-01</Text>
+                        </View>
+                        <View style={{flex:1,backgroundColor:'red',width:screenWidth/3,height:100}}>
+                        <Text>95-01</Text>
+                        </View>
+                        <View style={{flex:1,backgroundColor:'blue',width:screenWidth/3,height:100}}>
+                        <Text>95-01</Text>
+                        </View>
+                    </ScrollView>
+                    <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Vos itinéraires</Text>
+                    <ScrollView horizontal={true}>
+                        <View style={{flex:1,backgroundColor:'red',width:screenWidth/3,height:100}}>
+                        <Text>10 arpents - Jussieu</Text>
+                        </View>
+                        <View style={{flex:1,backgroundColor:'blue',width:screenWidth/3,height:100}}>
+                        <Text>Les Halles - Bibliothèque François Mittérant</Text>
+                        </View>
+                        <View style={{flex:1,backgroundColor:'red',width:screenWidth/3,height:100}}>
+                        <Text>Noisy-Champs - Gare de Lyon</Text>
+                        </View>
+                        <View style={{flex:1,backgroundColor:'blue',width:screenWidth/3,height:100}}>
+                        <Text>Strasbourg Saint-Denis - Odéon</Text>
+                        </View>
+                    </ScrollView>
                 </View>
             )
         }
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 2,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignSelf: 'stretch',
+    },
+    home: {
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      color: "#666666",
+      fontWeight: 'bold',
+      fontSize: 18,
+      padding: 10,
+      flex:0.8,
+    },
+    ImageStyle: {
+      margin: 10,
+      paddingLeft: 5,
+      resizeMode : 'contain',
+      alignItems: 'center',
+      maxHeight:20,
+      flex:0.1,
+    },
+    searchBar: {
+      backgroundColor: '#ffffff',
+      borderRadius: 5,
+      //IOS
+      shadowOpacity: 0.3,
+      shadowRadius: 50,
+      shadowOffset: {
+          height: 0,
+          width: 0
+      },
+      //android
+      elevation: 7,
+      flex:0.9,
+      flexDirection:'row'
+    },
+  });
+  
 
 export default HomePage;
