e91859e75b97ed2ff381ffdbed524d422d5a0315
Redha AISSAOUI <redha955@hotmail.fr>
2018-06-19 10:02:45 +0200

    Shadow modification parameter for IOS

--- components/Views/HomePage.js
+++ components/Views/HomePage.js
@@ -172,10 +172,10 @@ const styles = StyleSheet.create({
         backgroundColor: '#ffffff',
         borderRadius: 5,
         //IOS
-        shadowOpacity: 0.3,
-        shadowRadius: 50,
+        shadowOpacity: 0.2,
+        shadowRadius: 7,
         shadowOffset: {
-            height: 0,
+            height: 4,
             width: 0
         },
         //android
@@ -199,10 +199,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         borderRadius: 5,
         //IOS
-        shadowOpacity: 0.3,
-        shadowRadius: 50,
+        shadowOpacity: 0.2,
+        shadowRadius: 7,
         shadowOffset: {
-            height: 0,
+            height: 4,
             width: 0
         },
         //android
@@ -235,12 +235,12 @@ const styles = StyleSheet.create({
       backgroundColor: '#ffffff',
       borderRadius: 5,
       //IOS
-      shadowOpacity: 0.3,
-      shadowRadius: 50,
-      shadowOffset: {
-          height: 0,
-          width: 0
-      },
+        shadowOpacity: 0.2,
+        shadowRadius: 7,
+        shadowOffset: {
+            height: 4,
+            width: 0
+        },
       //android
       elevation: 7,
       flex:0.9,
